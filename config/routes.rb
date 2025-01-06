4717ed82a290363e22afd8765b536596a13be125
xebros <rorban1@live.com>
2025-01-05 19:16:57 -0600

    Added Model and Controller for Posts - Updated routes - Root path is posts#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  resources :posts, only: [ :new, :create, :index ]
+
+  # get "posts/new"
+  # get "posts/create"
+  # get "posts/index"
+
   devise_for :users
   # root "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
@@ -12,5 +18,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "posts#index"
 end
